1e56bb673856aaad1abe18f8b1da763f08a2ab5a
Junior Pires <jpirees@hotmail.com>
2022-09-11 23:16:47 -0300

    Create essentials.sh

---
+++ ubuntu/essentials.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade
+
+sudo apt install apt-transport-https \
+ca-certificates \
+gnupg-agent \
+software-properties-common \
+wget \
+unzip \
+build-essential \
+checkinstall \
+libssl-dev \
+fzf -y
