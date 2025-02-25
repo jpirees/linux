5b9de2094615dae08333367f68b05b0a21621a8a
Junior Pires <jpirees@hotmail.com>
2025-02-25 09:26:49 -0300

    Update develop.sh

--- ubuntu/develop.sh
+++ ubuntu/develop.sh
@@ -18,5 +18,26 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 sudo chmod 777 /var/run/docker.sock
 
-echo 'Installing FireCode font'
-sudo apt install fonts-firacode
+echo 'installing docker-buildx' 
+sudo apt install docker-buildx
+
+echo 'installing docker-compose' 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'installing kubectl' 
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo 'installing kubectx kubens' 
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+echo 'installing k9s' 
+curl -L "https://github.com/derailed/k9s/releases/download/v0.32.7/k9s_$(uname -s)_amd64.tar.gz" -o k9s
+tar -xf k9s
+chmod +x k9s
+sudo mv ./k9s /usr/local/bin/k9s
